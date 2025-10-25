876b027dfec450cec59e05f6da22d53920c610b3
DearYuto <reasons@kakao.com>
2025-10-25 10:26:02 +0900

    feat(proxy): 유저 권한에 따른 리다이렉트 프록시 로직 추가

---
+++ proxy.ts
@@ -0,0 +1,37 @@
+import { getAuthStatus } from "@/lib/utils/auth/getAuthStatus";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const ROUTES = Object.freeze({
+  HOME: "/",
+  LOGIN: "/login",
+  POSTS: "/posts",
+});
+
+export function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const redirectTo = (url: string) => {
+    return NextResponse.redirect(new URL(url, request.url));
+  };
+
+  const { isAuthenticated } = getAuthStatus(request);
+
+  if (pathname === ROUTES.HOME) {
+    return redirectTo(isAuthenticated ? ROUTES.POSTS : ROUTES.LOGIN);
+  }
+
+  if (isAuthenticated && pathname === ROUTES.LOGIN) {
+    return redirectTo(ROUTES.POSTS);
+  }
+
+  if (!isAuthenticated && pathname === ROUTES.POSTS) {
+    return redirectTo(ROUTES.LOGIN);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/login", "/posts/:path*"],
+};
