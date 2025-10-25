6d25f0db3e380244461cbe4b5e166f5b742cd8fe
DearYuto <reasons@kakao.com>
2025-10-25 17:00:14 +0900

    feat(Post): 타이핑 추가

--- src/lib/types/post.ts
+++ src/lib/types/post.ts
@@ -3,11 +3,19 @@ import { Tag } from "./tag";
 export type PostCategory = "NOTICE" | "QNA" | "FREE";
 
 export interface Post {
-	id: number;
-	title: string;
-	content?: string;
-	category: PostCategory;
-	author: string;
-	createdAt: string;
-	tags?: Tag[];
+  id: number;
+  userId: string;
+  title: string;
+  body?: string;
+  category: PostCategory;
+  tags?: Tag[];
+  createdAt: string;
+}
+
+export interface PostFormData {
+  id: number;
+  title: string;
+  content: string;
+  category: PostCategory;
+  tags?: Tag[];
 }
