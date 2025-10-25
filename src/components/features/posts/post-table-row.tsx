c15f69008b8cdd658d98923b72cb8740d83493fe
DearYuto <reasons@kakao.com>
2025-10-25 17:23:03 +0900

    chore(PostTableRow): 프롭스 네이밍 수정

--- src/components/features/posts/post-table-row.tsx
+++ src/components/features/posts/post-table-row.tsx
@@ -2,21 +2,21 @@ import { TableCell, TableRow } from "@/components/ui/table";
 import { Post } from "@/lib/types/post";
 import Link from "next/link";
 
-const PostTableRow = ({ id, title, category, author, createdAt }: Post) => {
-	const postURL = `/posts/${id}`;
+const PostTableRow = ({ id, title, category, userId, createdAt }: Post) => {
+  const postURL = `/posts/${id}`;
 
-	return (
-		<TableRow>
-			<TableCell>{category}</TableCell>
-			<TableCell>
-				<Link href={postURL} className="hover:underline hover:text-primary">
-					{title}
-				</Link>
-			</TableCell>
-			<TableCell>{author}</TableCell>
-			<TableCell className="text-muted-foreground">{createdAt}</TableCell>
-		</TableRow>
-	);
+  return (
+    <TableRow>
+      <TableCell>{category}</TableCell>
+      <TableCell>
+        <Link href={postURL} className="hover:underline hover:text-primary">
+          {title}
+        </Link>
+      </TableCell>
+      <TableCell>{userId}</TableCell>
+      <TableCell className="text-muted-foreground">{createdAt}</TableCell>
+    </TableRow>
+  );
 };
 
 export default PostTableRow;
