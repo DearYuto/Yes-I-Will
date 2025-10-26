d14f34f43574f73d0a512d99c6e80fcffbe87dd7
DearYuto <reasons@kakao.com>
2025-10-26 14:49:47 +0900

    feat: 하이드레이션 미스매치로 인해 다이나믹 로드하도록 로직 추가, 레이아웃 쉬프트 현상으로 UX가 저하되지 않도록 스켈레톤 처리

--- src/components/features/posts/post-toolbar.tsx
+++ src/components/features/posts/post-toolbar.tsx
@@ -1,7 +1,39 @@
 "use client";
 
-import { PostSearchBar } from "./post-search-bar";
-import { PostFilters } from "./post-filters";
+import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const SearchBarSkeleton = () => (
+  <div className="flex gap-2 w-full max-w-md">
+    <Skeleton className="flex-1 h-9" />
+    <Skeleton className="w-9 h-9" />
+  </div>
+);
+
+const FiltersSkeleton = () => (
+  <div className="flex gap-2 flex-wrap">
+    <Skeleton className="w-[140px] h-9" />
+    <Skeleton className="w-[140px] h-9" />
+    <Skeleton className="w-[140px] h-9" />
+  </div>
+);
+
+const PostSearchBar = dynamic(
+  () =>
+    import("./post-search-bar").then((mod) => ({ default: mod.PostSearchBar })),
+  {
+    ssr: false,
+    loading: () => <SearchBarSkeleton />,
+  }
+);
+
+const PostFilters = dynamic(
+  () => import("./post-filters").then((mod) => ({ default: mod.PostFilters })),
+  {
+    ssr: false,
+    loading: () => <FiltersSkeleton />,
+  }
+);
 
 export const PostToolbar = () => {
   return (
